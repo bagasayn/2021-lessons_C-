798201ea4f2d92e77bffca98e67b7d71b33c7563
bagasayn <bagasayn@gmail.com>
2021-10-07 15:34:11 +0300

    lab3

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -1,6 +1,16 @@
 #include "lab3.h"
 
 int main () {
-    std::cout << "1";
+    int arr[] = {1,2,3,4,5};
+
+    int *p = arr;
+
+    int *q = &arr[0];
+    int *s = ++p;
+
+    ptrdiff_t dif = q - s ;
+    
+    std::cout << dif; 
+
     return 0;
 }
